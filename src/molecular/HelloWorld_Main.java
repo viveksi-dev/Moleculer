bb9818d22a5172c7b65aebd1d852b8f1258c4305
viveksi <viveksi@vfsglobal.com>
2020-03-13 12:19:22 +0530

    second commit after chances

--- src/molecular/HelloWorld_Main.java
+++ src/molecular/HelloWorld_Main.java
@@ -10,12 +10,15 @@ public class HelloWorld_Main extends HelloWorld{
 	
 
 	@Test
-	public void Molecular() {
+	public void Molecular() throws InterruptedException {
 		driver.findElement(By.xpath(prop.getProperty("GreeterService"))).click();
+		Thread.sleep(3000);
 		driver.findElement(By.xpath(prop.getProperty("ExecuteButton"))).click();
+		Thread.sleep(3000);
 		String Moleculertxt = driver.findElement(By.xpath(prop.getProperty("HelloMoleculerText"))).getText();
+		System.out.println(Moleculertxt);
 		if(Moleculertxt.equalsIgnoreCase("Hello Moleculer")) {
-			System.out.println("Successfully Found the texts" + "Printing text in the output" +Moleculertxt);
+			System.out.println("Successfully Found the text:" +"       "  +"Printing text in the output:"+ "     " +Moleculertxt);
 		}else {
 			System.out.println("Text not found");
 		}
